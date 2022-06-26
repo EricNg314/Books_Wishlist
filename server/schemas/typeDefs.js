64ad2e8fe93554195b4604c557465d8335e72c05
Okens <Ng.Eric314@gmail.com>
2022-06-25 20:45:45 -0700

    Adding books type, user book parameters, and saveBookInput mutation.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,9 +2,20 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID
+    _id: ID!
     username: String
-    email: String
+    email: String,
+    bookCount: Int,
+    savedBooks: [Book]
+  }
+  type Book {
+    _id: ID!
+    bookId: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
   }
   type Auth {
     token: ID!
@@ -16,6 +27,15 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+    saveBook(body: saveBookInput!): User
+  }
+  input saveBookInput {
+    bookId: String!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
   }
 `;
 
