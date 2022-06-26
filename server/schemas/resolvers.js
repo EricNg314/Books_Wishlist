0f5c698da243be411260d3b298b3dbc6e4b121d4
Okens <Ng.Eric314@gmail.com>
2022-06-25 17:05:03 -0700

    Adding login mutation for server and updating client to use login mutation.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -21,9 +21,24 @@ const resolvers = {
       const token = signToken(user);
 
       return { token, user };
-    }
-  }
-};
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
 
 module.exports = resolvers;
